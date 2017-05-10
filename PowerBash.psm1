3241fb4ec1c44f3b65eda68d5eb962c37fa8b59c
Jimmy Cleary <jimmehc@gmail.com>
2017-05-09 20:59:42 -0700

    Initial modifications to simplify PowerBash

--- PowerBash.psm1
+++ PowerBash.psm1
@@ -58,124 +58,58 @@ function Add-BashWrappers
     )
 
     $allPSCommandsHash = @{}
-    foreach($c in ((Get-Command *).Name -replace "\.exe$","" | Where-Object { $Overwrite -notcontains $_ })){ $allPSCommandsHash[$c] = $true; }
+    foreach($c in ((Get-Command *).Name -replace "\.exe$",""))
+    {
+         if($Overwrite -notcontains $_ )
+         { 
+             $allPSCommandsHash[$c] = $true;
+         }
+    }
 
-    $BashApps | Where-Object { !$allPSCommandsHash[$_.Name] } -pv app |
-        % { 
-              New-Item -Path function: -Name $app.Name -Value {
-                  begin
-                  {
-                      $allPipeArgs = @()
+    foreach($app in $BashApps)
+    {
+        if(!$allPSCommandsHash[$app.Name])
+        {
+            New-Item -Path function: -Name $app.Name -Value {
+                begin
+                {
+                    $filteredArgs = New-Object System.Collections.ArrayList
+                    foreach($a in $args)
+                    {
+                        if($a -ne "--%")
+                        {
+                            $filteredArgs += $a
+                        }
+                    }
 
-                      $hasPipeForward = $false
-                      $invocationInfos = (Get-PSCallStack).InvocationInfo
-                      foreach($invocation in ($invocationInfos | Select-Object -SkipLast 1))
-                      {
-                          if($hasPipeForward)
-                          {
-                              break
-                          }
+                    $bashCommandStr = "$($app.Path) $filteredArgs"
+                    $bashArgs = "-c",$bashCommandStr
+                }
 
-                          $hasPipeForward = ($invocation.PipelinePosition -ne $invocation.PipelineLength)
-                          if(!$hasPipeForward)
-                          {
-                              $ast = [Management.Automation.Language.Parser]::ParseInput($invocation.Line, [ref]$null, [ref]$null)
-                              $commandOffset = $invocation.OffsetInLine
-                              $pipelineAst = $ast.EndBlock.Statements | Where-Object { $_.Extent.StartColumnNumber -le $commandOffset -and $_.Extent.EndColumnNumber -gt $commandOffset }
-                              if($pipelineAst -isnot [System.Management.Automation.Language.PipelineAst])
-                              {
-                                  $hasPipeForward = $true
-                              }
-                              else
-                              {
-                                  $commandAst = $pipelineAst.PipelineElements | Where-Object { $_.Extent.StartColumnNumber -le $commandOffset -and $_.Extent.EndColumnNumber -gt $commandOffset }
+                end 
+                {
+                    if(@($input).Count -gt 0)
+                    {
+                        $input.Reset()
+                        $input | & "bash.exe" $bashArgs
+                    }
+                    else
+                    {
+                        & "bash.exe" $bashArgs
+                    }
 
-                                  $hasPipeForward = (($commandAst -isnot [System.Management.Automation.Language.CommandAst]) -or ($commandAst.Redirections.Length -gt 0))
-                              }
-                          }
-                      }
-                  }
+                    if(($filteredArgs.Length -ge 3 -and $app.Name -eq "sudo" -and ("apt-get","apt") -contains $filteredArgs[0] -and $filteredArgs[1] -eq "install") -or
+                            ($filteredArgs.Length -ge 2 -and ("apt-get","apt") -contains $app.Name -and $filteredArgs[0] -eq "install"))
+                    {
+                        Import-Module $script:thisModulePath -Force -Global -DisableNameChecking
+                    }
+                }
+            }.GetNewClosure()
 
-                  process
-                  {
-                      foreach($i in $input)
-                      {
-                          $hasPipeArgs = $true
-                          $allPipeArgs += $i
-                      }
-                  }
-
-                  end
-                  {
-                      try
-                      {
-                          $bashArgs = @()
-                          foreach($a in $args)
-                          {
-                              if($a -ne "--%")
-                              {
-                                  $bashArgs += $a
-                              }
-                          }
-
-                          $bashCommandStr = "$($app.Path) $bashArgs".Trim()
-                          if($hasPipeArgs)
-                          {
-                              $tempStdIn = (New-TemporaryFile).FullName
-                              [IO.File]::WriteAllLines($tempStdIn, $allPipeArgs)
-
-                              $linuxStdIn = ConvertTo-LinuxPath $tempStdIn
-                              $bashCommandStr += " <$linuxStdIn"
-                          }
-
-                          if($hasPipeForward)
-                          {
-                              $tempStdOut = (New-TemporaryFile).FullName
-                              $tempStdErr = (New-TemporaryFile).FullName
-                              $linuxStdOut = ConvertTo-LinuxPath $tempStdOut
-                              $linuxStdErr = ConvertTo-LinuxPath $tempStdErr
-
-                              $bashCommandStr += " >$linuxStdOut 2>$linuxStdErr"
-                              $cmdCommandStr = "bash.exe -c `"$bashCommandStr`""
-                              $p = start-process "cmd" -ArgumentList "/c",$cmdCommandStr -WindowStyle Hidden -PassThru
-                              while(!$p.HasExited){ Start-Sleep -Milliseconds 1 }
-                          }
-                          else
-                          {
-                              bash.exe -c "$bashCommandStr"
-                          }
-                      }
-                      finally
-                      {
-                          if($tempStdOut -and (Test-Path $tempStdOut))
-                          {
-                              Get-Content $tempStdOut
-                              Remove-Item $tempStdOut
-                          }
-
-                          if($tempStdErr -and (Test-Path $tempStdErr))
-                          {
-                              Get-Content $tempStdErr | Write-Error
-                              Remove-Item $tempStdErr
-                          }
-
-                          if($tempStdIn -and (Test-Path $tempStdIn))
-                          {
-                              Remove-Item $tempStdIn
-                          }
-                      }
-
-                      if(($bashArgs.Length -ge 3 -and $app.Name -eq "sudo" -and $bashArgs[0] -eq "apt-get" -and $bashArgs[1] -eq "install") -or
-                          ($bashArgs.Length -ge 2 -and $app.Name -eq "apt-get" -and $bashArgs[0] -eq "install"))
-                      {
-                          Import-Module $script:thisModulePath -Force -Global 3>$null
-                      }
-                  }
-             }.GetNewClosure()
-            
             Export-ModuleMember -Function $app.Name
             $allPSCommandsHash[$app.Name] = $true
         }
+    }
 }
 
 $script:allBashApps = Get-AllBashApps
